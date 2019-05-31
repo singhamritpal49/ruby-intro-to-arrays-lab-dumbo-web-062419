fa46e07e7060067aa54da5c8be239678a36ba3f6
Amritpal Singh <singhamritpal49@gmail.com>
2019-05-31 18:24:21 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,36 @@
+def instantiate_new_array
+  instantiate_new_array = []
+return instantiate_new_array
+end
+
+def array_with_two_elements
+  array_with_two_elements = ["Amrit","singh"]
+  return array_with_two_elements
+end
+
+def first_element(array)
+  array = ["Welcome to New York","Welcome to Seattle"]
+  return array.first
+end
+def third_element(third_element)
+array = ["Welcome to New York","Welcome to Seattle","Welcome to California","Style"]
+return array[3]
+end
+
+def last_element(last_element)
+array = ["Welcome to New York","Welcome to Seattle","Welcome to California","Out of The Woods"]
+return array[-1]
+end
+def first_element_with_array_methods(array)
+  array = ["Thailand","Welcome to Seattle","Welcome to California","Out of The Woods"]
+  return array.first
+end
+def last_element_with_array_methods(array)
+  array = ["Thailand","Welcome to Seattle","Welcome to California","Myanmar"]
+  return array[-1]
+end
+def length_of_array(length)
+  array = ["Thailand","Welcome to Seattle","Welcome to California","Myanmar","Thailand","Welcome to Seattle","Welcome to California","Myanmar"]
+  return array.length
+
+end
