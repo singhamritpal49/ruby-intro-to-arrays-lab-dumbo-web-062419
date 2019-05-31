7a9094dba6680fbc5a4048d7827c1c37a50c9155
Amritpal Singh <singhamritpal49@gmail.com>
2019-05-31 18:22:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -29,3 +29,8 @@ def last_element_with_array_methods(array)
   array = ["Thailand","Welcome to Seattle","Welcome to California","Myanmar"]
   return array[-1]
 end
+def length_of_array(length)
+  array = ["Thailand","Welcome to Seattle","Welcome to California","Myanmar"]
+  return array.length
+
+end
