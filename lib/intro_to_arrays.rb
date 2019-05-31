39bcb4b316bf041c69d27e25f6fd47950f966d6d
Amritpal Singh <singhamritpal49@gmail.com>
2019-05-31 17:48:56 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,14 @@
+def instantiate_new_array
+  instantiate_new_array = []
+return instantiate_new_array
+end
+
+def array_with_two_elements
+  array_with_two_elements = ["Amrit","singh"]
+  return array_with_two_elements
+end
+
+def first_element(array)
+  
+
+end
